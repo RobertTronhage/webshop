da7332047edd330b39bf1ef141c7a2933dc7f58b
MagNording <magnus_nording@msn.com>
2024-03-25 12:54:36 +0100

    User update

--- src/main/java/se/tronhage/webshop/Entity/User.java
+++ src/main/java/se/tronhage/webshop/Entity/User.java
@@ -1,6 +1,8 @@
 package se.tronhage.webshop.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import se.tronhage.webshop.Enums.Role;
 
 import java.util.ArrayList;
@@ -13,10 +15,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is invalid")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 
+    @NotBlank(message = "Role is required") // ? On Admin side
     @Enumerated(EnumType.STRING)
     private Role role;
 
