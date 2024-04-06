f68ee2cd20722e2c454224c3ffccda80d3c33eb6
Magnus Nording <magnus.nording@iths.se>
2024-04-06 13:04:09 +0200

    UserService, new variable name userFromDb and userUpdates User getUser() got its own method

--- src/main/java/se/tronhage/webshop/services/UserService.java
+++ src/main/java/se/tronhage/webshop/services/UserService.java
@@ -3,9 +3,7 @@ package se.tronhage.webshop.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import se.tronhage.webshop.entity.Order;
 import se.tronhage.webshop.entity.User;
-import se.tronhage.webshop.enums.OrderStatus;
 import se.tronhage.webshop.enums.Role;
 import se.tronhage.webshop.repository.UserRepo;
 import se.tronhage.webshop.exceptions.UserAlreadyExistsException;
@@ -18,18 +16,17 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    UserRepo userRepo;
+    private final UserRepo userRepo;
 
     @Autowired
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
 
-    public User registerNewUser(String firstName, String lastName, String email, String address, String username, String password) {
+    public void registerNewUser(String firstName, String lastName, String email, String address, String username, String password) {
         if(userRepo.existsByUsername(username)) {
             throw new UserAlreadyExistsException("Username already in use.");
         }
-
         User newUser = new User();
         newUser.setFirstName(firstName);
         newUser.setLastName(lastName);
@@ -39,35 +36,41 @@ public class UserService {
         newUser.setPassword(password);
         newUser.setRole(Role.USER);
         //newUser.setShoppingBasket(new ShoppingBasket()); // ny tom varukorg för användaren
-        return userRepo.save(newUser);
+        userRepo.save(newUser);
     }
 
     @Transactional
-    public void updateUser(User updatedUser, String newPassword) {
+    public void updateUser(User userUpdates, String newPassword) {
         // Retrieve the user from the database based on the provided ID
-        Optional<User> optionalUser = userRepo.findById(updatedUser.getId());
+        Optional<User> optionalUser = userRepo.findById(userUpdates.getId());
 
         if (optionalUser.isPresent()) {
-            User existingUser = optionalUser.get();
-
-            // Update the user details with the values from the updatedUser object
-            existingUser.setFirstName(updatedUser.getFirstName());
-            existingUser.setLastName(updatedUser.getLastName());
-            existingUser.setEmail(updatedUser.getEmail());
-            existingUser.setAddress(updatedUser.getAddress());
-            existingUser.setUsername(updatedUser.getUsername());
-
-            // Uppdatera endast lösenordet om ett nytt har angetts
-            if (newPassword != null && !newPassword.trim().isEmpty()) {
-                existingUser.setPassword(newPassword);
-            }
+            User userFromDb = getUser(userUpdates, newPassword, optionalUser);
             // Save the updated user back to the database
-            userRepo.save(existingUser);
+            userRepo.save(userFromDb);
         } else {
             // Handle the case where the user with the provided ID does not exist
-            throw new UserNotFoundException("User not found with ID: " + updatedUser.getId());
+            throw new UserNotFoundException("User not found with ID: " + userUpdates.getId());
         }
     }
+
+    private static User getUser(User userUpdates, String newPassword, Optional<User> optionalUser) {
+        User userFromDb = optionalUser.get();
+
+        // Update the user details with the values from the userUpdates object
+        userFromDb.setFirstName(userUpdates.getFirstName());
+        userFromDb.setLastName(userUpdates.getLastName());
+        userFromDb.setEmail(userUpdates.getEmail());
+        userFromDb.setAddress(userUpdates.getAddress());
+        userFromDb.setUsername(userUpdates.getUsername());
+
+        // Uppdatera endast lösenordet om ett nytt har angetts
+        if (newPassword != null && !newPassword.trim().isEmpty()) {
+            userFromDb.setPassword(newPassword);
+        }
+        return userFromDb;
+    }
+
 
     public boolean authUser(String username, String password){
         Optional<User> user = userRepo.findByUsername(username);
